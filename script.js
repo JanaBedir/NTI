f376595183c820657cc88a6f1e23b1326a6b714b
JanaBedir <jony2452006@gmail.com>
2026-07-12 21:33:52 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*******************************TASK ONE********************************/
+let firstNum = +prompt("Enter the first number?");
+let secondNum = +prompt("Enter the second number?");
+
+console.log(`The sum of two numbers: ${firstNum + secondNum}`);
+console.log(`The sub of two numbers: ${firstNum - secondNum}`);
+console.log(`The mul of two numbers: ${firstNum * secondNum}`);
+console.log(`The div of two numbers: ${firstNum / secondNum}`);
+
+/*******************************TASK TWO********************************/
+let firstName = prompt("Enter your first name?");
+let lastName = prompt("Enter your second name?");
+let age = +prompt("Enter your age?");
+
+console.log(`Hello ${firstName} ${lastName}, your age is ${age}`);
